b18e3cd4de6dc728b8ecc14aa8064fb88ac36db5
Darren Ramsook <43309308+DarrenR96@users.noreply.github.com>
2021-11-11 12:18:20 +0000

    Updated to be more efficient
    Used np.frombuffer and added option to upsample the UV channels

--- yuv420.py
+++ yuv420.py
@@ -1,14 +1,22 @@
 import numpy as np
 
-def readYUVtoArr(file,width = 1280,height = 720):
-    px = width*height
-    YUV = np.fromfile(file,dtype='uint8')
-    frames = int(YUV.shape[0]/(px+((2*px)/4)))
+def readYUV420(name: str, resolution: tuple, upsampleUV: bool = False):
+    height = resolution[0]
+    width = resolution[1]
+    bytesY = int(height * width)
+    bytesUV = int(bytesY/4)
     Y = []
     U = []
     V = []
-    for frame in range(0,frames):
-        Y.append(YUV[int((px + 2*(px/4))*frame):int((px + 2*(px/4))*frame+px)].reshape(height,width))
-        U.append(YUV[int((px + 2*(px/4))*frame+px):int((px + 2*(px/4))*frame+px+(px/4))].reshape(height//2,width//2))
-        V.append(YUV[int((px + 2*(px/4))*frame+px+(px/4)):int((px + 2*(px/4))*frame+px+2*(px/4))].reshape(height//2,width//2))
-    return np.array(Y), np.array(U), np.array(V)
+    with open(name,"rb") as yuvFile:
+        while (chunkBytes := yuvFile.read(bytesY + 2*bytesUV)):
+            Y.append(np.reshape(np.frombuffer(chunkBytes, dtype=np.uint8, count=bytesY, offset = 0), (width, height)))
+            U.append(np.reshape(np.frombuffer(chunkBytes, dtype=np.uint8, count=bytesUV, offset = bytesY),  (width//2, height//2)))
+            V.append(np.reshape(np.frombuffer(chunkBytes, dtype=np.uint8, count=bytesUV, offset = bytesY + bytesUV), (width//2, height//2)))
+    Y = np.stack(Y)
+    U = np.stack(U)
+    V = np.stack(V)
+    if upsampleUV:
+        U = U.repeat(2, axis=1).repeat(2, axis=2)
+        V = V.repeat(2, axis=1).repeat(2, axis=2)
+    return Y, U, V
