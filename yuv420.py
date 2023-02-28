ddd72c3aa5d45d09e5fdc0572d66b6d74ce3af74
Darren Ramsook <43309308+DarrenR96@users.noreply.github.com>
2023-02-28 11:40:42 +0000

    Update yuv420.py

--- yuv420.py
+++ yuv420.py
@@ -101,3 +101,24 @@ def writeYUV420(name: str, Y, U, V, downsample=True):
         towrite.extend(V[i].tobytes())
     with open(name, "wb") as destination:
         destination.write(towrite)
+        
+def RGB2YUV(rgb):
+    m = np.array([[ 0.29900, -0.16874,  0.50000],
+                 [0.58700, -0.33126, -0.41869],
+                 [ 0.11400, 0.50000, -0.08131]])
+    yuv = np.dot(rgb,m)
+    yuv[:,:,:,1:]+=128.0
+    yuv = np.clip(yuv,0,255)
+    return yuv
+
+def YUV2RGB(yuv):
+    m = np.array([[ 1.0, 1.0, 1.0],
+                 [-0.000007154783816076815, -0.3441331386566162, 1.7720025777816772],
+                 [ 1.4019975662231445, -0.7141380310058594 , 0.00001542569043522235] ])
+    
+    rgb = np.dot(yuv,m)
+    rgb[:,:,:,0]-=179.45477266423404
+    rgb[:,:,:,1]+=135.45870971679688
+    rgb[:,:,:,2]-=226.8183044444304
+    rgb = np.clip(rgb,0,255)
+    return rgb
